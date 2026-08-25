a207ce98295ffda78a898b8960bb9d51c6031755
ViPro (京京) <VdustR@gmail.com>
2026-08-25 10:53:10 +0800

    fix(deps): restore MIT PrimeVue dependencies (#10)

--- src/components/ui/ThemeCombobox.vue
+++ src/components/ui/ThemeCombobox.vue
@@ -55,7 +55,7 @@ watch(query, () => {
         :delay="0"
         :loading="loading"
         @complete="onComplete"
-        @option-select="onItemSelect"
+        @item-select="onItemSelect"
         @update:model-value="
           (val: string | Theme) => {
             if (typeof val === 'string') query = val;
